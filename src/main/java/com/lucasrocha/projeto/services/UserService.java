69c8bd77bd2cd08d58e1dd7e8e3f162f94731193
Lucas Rocha <lucasrocha478@gmail.com>
2024-06-18 11:04:56 -0300

    Service layer, component registration

---
+++ src/main/java/com/lucasrocha/projeto/services/UserService.java
@@ -0,0 +1,27 @@
+package com.lucasrocha.projeto.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.lucasrocha.projeto.entities.User;
+import com.lucasrocha.projeto.repository.UserRepository;
+
+@Service
+public class UserService {
+
+	@Autowired
+	private UserRepository repository;
+
+	public List<User> findAll() {
+		return repository.findAll();
+	}
+
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+
+}
