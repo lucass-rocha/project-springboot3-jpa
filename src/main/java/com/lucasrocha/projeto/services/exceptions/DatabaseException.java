25b8fdc02b40f726290c6dc688891ac2d8f059d7
Lucas Rocha <lucasrocha478@gmail.com>
2024-07-04 10:28:04 -0300

    Exception handling - delete

---
+++ src/main/java/com/lucasrocha/projeto/services/exceptions/DatabaseException.java
@@ -0,0 +1,14 @@
+package com.lucasrocha.projeto.services.exceptions;
+
+public class DatabaseException extends RuntimeException{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public DatabaseException(String msg) {
+		super(msg);
+		
+	}
+}
