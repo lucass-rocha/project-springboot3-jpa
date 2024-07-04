3c9c8165c65711df799bf63b2e5f8a9878d75a4f
Lucas Rocha <lucasrocha478@gmail.com>
2024-07-04 10:00:39 -0300

    Exception handling - findById

--- src/main/java/com/lucasrocha/projeto/services/UserService.java
+++ src/main/java/com/lucasrocha/projeto/services/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import com.lucasrocha.projeto.entities.User;
 import com.lucasrocha.projeto.repository.UserRepository;
+import com.lucasrocha.projeto.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class UserService {
@@ -21,7 +22,7 @@ public class UserService {
 
 	public User findById(Long id) {
 		Optional<User> obj = repository.findById(id);
-		return obj.get();
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
 
 	
